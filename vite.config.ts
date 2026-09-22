fe0d21ed1d38820b7dddbfa10172560167250b55
Black-Alpha112 <jado10ali@gmail.com>
2026-09-22 23:45:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,6 @@ export default defineConfig(async ({ mode }) => {
     plugins,
     envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
     define: processEnvDefines,
-    base: "Med-Care-info"
+    base: "/Med-Care-info",
   };
 })
